033ccaba14ae8e2070e8c8b4f27b6ae3c324dd24
IGOR RHAMON CARDOSO DO NASCIMENTO <igor-rh@hotmail.com>
2022-06-16 08:45:04 -0300

    Removendo o VoxelDog

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -1,14 +1,14 @@
 import Head from 'next/head'
-import dynamic from 'next/dynamic'
+// import dynamic from 'next/dynamic'
 import NavBar from '../navbar'
 import { Box, Container } from '@chakra-ui/react'
 import Footer from '../footer'
-import VoxelDogLoader from '../voxel-dog-loader'
+// import VoxelDogLoader from '../voxel-dog-loader'
 
-const LazyVoxelDog = dynamic(() => import('../voxel-dog'), {
-  ssr: false,
-  loading: () => <VoxelDogLoader />
-})
+// const LazyVoxelDog = dynamic(() => import('../voxel-dog'), {
+//   ssr: false,
+//   loading: () => <VoxelDogLoader />
+// })
 
 const Main = ({ children, router }) => {
   return (
@@ -34,9 +34,10 @@ const Main = ({ children, router }) => {
 
       <NavBar path={router.asPath} />
 
-      <Container maxW="container.md" pt={14}>
-        <LazyVoxelDog />
-
+      <Container maxW="container.md" pt={24} >
+        {/* <LazyVoxelDog /> */}
+        
+        
         {children}
 
         <Footer />
